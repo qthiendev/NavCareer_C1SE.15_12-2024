e85e41a754c70219f12ec3e24b8f6dab1cdd1114
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-09-20 17:43:37 +0700

    refactor path rule

--- server/src/mainRoute.js
+++ server/src/mainRoute.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
-const { queryDB } = require('./services/database/queryDB');
-const { signIn } = require("./controllers/authentications/signInController");
-const { getProfile } = require("./controllers/accountManagement/profileController");
+const { queryDB } = require('./apis/database/queryDBService');
 
 router.get("/", (req, res) => { res.json(''); });
-// router.get("/signin/get", signIn);
-// router.get("/profile/get", getProfile);
 
 router.get("/test/query", async (req, res) => {
     const { queryString } = req.query;
