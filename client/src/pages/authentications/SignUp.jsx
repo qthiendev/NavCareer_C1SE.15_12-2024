e7f0c7654cf31c75492324a1bbb623e6cc64d5f3
KhaiPhan164 <phancongkhai164@gmail.com>
2024-09-22 13:37:08 +0700

    Update Signup

---
+++ client/src/pages/authentications/SignUp.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './SignIn.css'; // Create a CSS file for styling
+function SignUp() {
+
+
+    return (
+        <div className="signin-container">
+           Sign Up
+        </div>
+    );
+}
+
+export default SignUp;
