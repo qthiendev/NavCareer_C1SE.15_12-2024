789e53ffe3172713a3d2ba412664e65f772e67b6
KhaiPhan164 <phancongkhai164@gmail.com>
2024-10-07 19:00:23 +0700

    update

---
+++ client/src/pages/courseManagement/ViewCourse.jsx
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-unused-vars
+import React, { useContext } from 'react'
+import "./ViewCourse.css"
+const ViewCourse = () => {
+    
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default ViewCourse
