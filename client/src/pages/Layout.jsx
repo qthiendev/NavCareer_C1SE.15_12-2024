c5e18a1876053b819e42c82915239e34277836f7
Trinh Quy Thien <trinhquythien.dev@gmail.com>
2024-10-15 19:24:09 +0700

    update

--- client/src/pages/Layout.jsx
+++ client/src/pages/Layout.jsx
@@ -79,7 +79,7 @@ function Layout() {
                         )}
                     </ul>
 
-                    <div className="auth">
+                    <div className="auth-links">
                         {isAuth ? (
                             <>
                                 <div className="notification-icon">
@@ -116,54 +116,8 @@ function Layout() {
             </main>
 
             <footer className="footer">
-                <div class="footer_container">
-                    <div class="logo-info">
-                        <div className="logo">
-                            <img src="img/Header/Logo.svg" alt="Logo" />
-                        </div>
-                        <ul>
-                            <li>
-                                <img src="img/Footer/mail_white_vector.svg" alt="" />
-                                <p>hello@navcareer.com</p>
-                            </li>
-                            <li>
-                                <img src="img/Footer/phone_white_vector.svg" alt="" />
-                                <p>+91 91813 23 2309</p>
-                            </li>
-                            <li>
-                                <img style={{ padding: "2px" }} src="img/Footer/location_white_vector.svg" alt="" />
-                                <p>Somewhere in the World</p>
-                            </li>
-                        </ul>
-                    </div>
-                    <div class="nav-links">
-                        <h3>Trọng tâm</h3>
-                        <ul>
-                            <li><a href="#">Hệ thống bài test</a></li>
-                            <li><a href="#">Khoá học</a></li>
-                            <li><a href="#">Tài nguyên hỗ trợ</a></li>
-                        </ul>
-                    </div>
-                    <div class="nav-links">
-                        <h3>Thông tin chi tiết</h3>
-                        <ul>
-                            <li><a href="#">Công ty</a></li>
-                            <li><a href="#">Sứ mệnh</a></li>
-                            <li><a href="#">Mục tiêu</a></li>
-                        </ul>
-                    </div>
-                    <div class="social-media">
-                        <h3>Kết nối với chúng tôi</h3>
-                        <ul>
-                            <li><a href="#"><img src="img/Footer/fb_vector.svg" alt="" /></a></li>
-                            <li><a href="#"><img src="img/Footer/linkedin_vector.svg" alt="" /></a></li>
-                            <li><a href="#"><img src="img/Footer/twitter_vector.svg" alt="" /></a></li>
-                        </ul>
-                    </div>
-                </div>
-                <p className='copyright'>&copy; C1SE.15</p>
+                <p className="copyright">&copy; C1SE.15</p>
             </footer>
-
         </div>
     );
 }
