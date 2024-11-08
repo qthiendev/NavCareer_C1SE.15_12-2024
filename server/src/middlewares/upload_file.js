6c3da07d964bee27171066adf228a14f91af5067
hoangphuc10x <phuc1092003@gmail.com>
2024-11-08 18:07:09 +0700

    done update avatar

--- server/src/middlewares/upload_file.js
+++ server/src/middlewares/upload_file.js
@@ -1,25 +1,37 @@
 const multer = require('multer');
 const path = require('path');
+const fs = require('fs');
 var appRoot = require('app-root-path');
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, appRoot+"/localResources/profiles/_1");
+        const userFolder = appRoot + "/localResources/profiles/_" + req.session.aid + "/";
+        
+        // Kiểm tra xem thư mục của người dùng đã tồn tại chưa, nếu chưa thì tạo mới
+        if (!fs.existsSync(userFolder)) {
+            fs.mkdirSync(userFolder, { recursive: true });
+        }
+
+        cb(null, userFolder);
     },
 
-    // By default, multer removes file extensions so let's add them back
     filename: function (req, file, cb) {
-        const id = req.session.aid; 
-        // cb(null, file.fieldname + '_' + {aid});
-        cb(null, 'avatar.png');
+        const avatarPath = appRoot + "/localResources/profiles/_" + req.session.aid + "/avatar.png";
+
+        // Kiểm tra nếu file avatar.png đã tồn tại, xóa file cũ
+        if (fs.existsSync(avatarPath)) {
+            fs.unlinkSync(avatarPath);
+        }
+
+        cb(null, 'avartar.png'); // Tên file cố định là avatar.png
     }
 });
 
 const imageFilter = function (req, file, cb) {
-    // Accept images only
+    // Chỉ chấp nhận các file hình ảnh
     if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-        req.fileValidationError = 'Only image files are allowed!';
-        return cb(new Error('Only image files are allowed!'), false);
+        req.fileValidationError = 'Chỉ chấp nhận file hình ảnh!';
+        return cb(new Error('Chỉ chấp nhận file hình ảnh!'), false);
     }
     cb(null, true);
 };
@@ -27,5 +39,7 @@ const imageFilter = function (req, file, cb) {
 const upload = multer({ storage: storage, fileFilter: imageFilter });
 
 module.exports = {
-    upload, storage, imageFilter,
-}
+    upload,
+    storage,
+    imageFilter,
+};
